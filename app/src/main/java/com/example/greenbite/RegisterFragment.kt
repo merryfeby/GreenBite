22515347ac03cda4b60ffbbcfd412edd3ee73480
Merry Febyanti <merryfebyanti@gmail.com>
2025-05-08 20:46:53 +0700

    Update import

--- app/src/main/java/com/example/greenbite/RegisterFragment.kt
+++ app/src/main/java/com/example/greenbite/RegisterFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.greenbite.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     lateinit var binding: FragmentRegisterBinding
