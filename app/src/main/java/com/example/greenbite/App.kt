6167d03a1a3bbc952f493afca2d3504eb5add92c
Merry Febyanti <merryfebyanti@gmail.com>
2025-05-22 21:04:55 +0700

    Add endpoints and create product view model

--- app/src/main/java/com/example/greenbite/App.kt
+++ app/src/main/java/com/example/greenbite/App.kt
@@ -1,10 +1,20 @@
 package com.example.greenbite
 
 import android.app.Application
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class App: Application() {
     companion object {
         lateinit var db: AppDatabase
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val retrofit = Retrofit.Builder().addConverterFactory(
+            MoshiConverterFactory.create(moshi)
+        ).baseUrl("http://10.0.2.2:8000/api/").build()
+
+        val retrofitService = retrofit.create(WebService::class.java)
     }
 
     override fun onCreate() {
---
+++ app/src/main/java/com/example/greenbite/ProductViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.greenbite
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class ProductViewModel : ViewModel() {
+    private val _currentCategory = MutableLiveData<String>("All")
+    val currentCategory: LiveData<String> = _currentCategory
+
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> = _products
+
+    val currentCategoryMenu: LiveData<List<Product>> = _currentCategory.switchMap { category ->
+        MutableLiveData<List<Product>>().apply {
+            viewModelScope.launch {
+                val fetchedProducts = if (category == "All") {
+                    App.retrofitService.getAllProducts()
+                } else {
+                    App.retrofitService.getProductsByCategory(category)
+                }
+                val temp = fetchedProducts.sortedBy { it.name }
+                _products.value = temp
+            }
+        }
+    }
+
+    fun setCategory(category: String) {
+        _currentCategory.value = category
+    }
+}
