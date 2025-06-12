4b8badea7ec5ea935dc8076fbbfc282143cbbcae
Darren Cahya Wijaya <cosmicbro08@gmail.com>
2025-06-12 23:54:39 +0700

    courier view

--- app/src/main/java/com/example/greenbite/courier/CourierActivity.kt
+++ app/src/main/java/com/example/greenbite/courier/CourierActivity.kt
@@ -3,19 +3,15 @@ package com.example.greenbite.courier
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.greenbite.R
+import com.example.greenbite.databinding.ActivityCourierBinding
 
 class CourierActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityCourierBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_courier)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_courier)
     }
 }
