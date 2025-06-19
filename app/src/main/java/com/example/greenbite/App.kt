13857a654e5332dafa2341bddfb9640f8ab74330
ValentinoTan <tanvalentino54@gmail.com>
2025-06-19 23:00:46 +0700

    admin bottom nav

--- app/src/main/java/com/example/greenbite/App.kt
+++ app/src/main/java/com/example/greenbite/App.kt
@@ -13,7 +13,7 @@ class App: Application() {
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val retrofit = Retrofit.Builder().addConverterFactory(
             MoshiConverterFactory.create(moshi)
-        ).baseUrl("http://10.0.2.2:8000/api/").build()
+        ).baseUrl("http://192.168.1.16:8000/api/").build()
 
         val retrofitService = retrofit.create(WebService::class.java)
     }
